6ac65e8baec83d70ccafcd16b47dd847041b6b28
Haozidd <285626941@qq.com>
2022-12-24 09:10:43 +0800

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <school></school>
+
+</template>
+
+
+<script>
+import School from './components/School.vue'
+
+export default {
+  name: 'App',
+  components: {
+    School
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ main.js
@@ -0,0 +1,8 @@
+import Vue from './js/vue'
+import App from "./app";
+
+new Vue({
+    el:'#root',
+    template:`<App></App>`,
+    components:{App},
+})
